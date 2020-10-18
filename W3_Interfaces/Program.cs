21cd91dd2de2376808237fa165126ab7866ce469
marco villanueva <villanuevamarco98@gmail.com>
2020-10-17 20:00:16 -0600

    interface IAnimal

--- W3_Interfaces/Program.cs
+++ W3_Interfaces/Program.cs
@@ -12,7 +12,7 @@ namespace W3_Interfaces
         interface Animal
         {
             void AnimalSound();//nterface method does not have a body
-            void run();//nterface method does not have a body
+            void run();//interface method does not have a body
         }
         //It is considered good practice to start with the letter "I" at the beginning of an interface, 
         //as it makes it easier for yourself and others to remember that it is an interface and not a class.
@@ -20,10 +20,31 @@ namespace W3_Interfaces
 
         //Note: Interfaces can contain properties and methods, but not fields.
 
+        //The interface must be "implemented" (kind of like inherited) by another class. 
+
+        //interface
+        interface IAnimal
+        {
+            void animalSound();
+        }
+
+        //To implement an interface, use the : symbol (just like with inheritance).
+
+        //Lemur 'implements' the IAnimal interface
+
+        class Lemur : IAnimal
+        {
+            public void animalSound()
+            {
+                //the body of animalSound() goes here
+                Console.WriteLine("The lemur goes...fwark!");
+            }
+        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Lemur myLemur = new Lemur();
+            myLemur.animalSound();
         }
     }
 }
